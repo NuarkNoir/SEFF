d3a2cc3f3efb57be59d9060cc4abce3053d5b5a0
Nuark <mrcluster@ya.ru>
2016-10-22 19:19:46 +0700

    Cleaning
    Поправил .gitignore

---
+++ pwxp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using NuarkNETOD;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace pwxp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("Newtonsoft.Json.dll")) return;
+            byte[] resf;
+            resf = Properties.Resources.Newtonsoft_Json;
+            File.WriteAllBytes("Newtonsoft.Json.dll", resf);
+        }
+
+        private void spth()
+        {
+            List<String> fic = new List<string>();
+            for (int num = 1; num < 10; num++)
+            {
+                string uri =
+                "https://query.yahooapis.com/v1/public/yql?q=select * from html where url='https://stories.everypony.ru/story/" + num + "/' and xpath='.//*[@id=\"story_title\"]'&format=json&env=store://datatables.org/alltableswithkeys&callback=";
+                string request = NuarkNeToD.GetResponse(uri);
+                dynamic titla = JsonConvert.DeserializeObject(request);
+                if (titla.query.count != 0)
+                {
+                    string toAdd = titla.query.results.h1.content;
+                    fic.Add(toAdd.Trim());
+                }
+            }
+            foreach (var name in fic)
+                lBo.Items.Add(name);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            spth();
+        }
+    }
+}
---
+++ x-path/yql_fic_response.cs
@@ -0,0 +1,20 @@
+﻿public class H1
+{
+    public string content { get; set; }
+}
+
+public class Results
+{
+    public H1 h1 { get; set; }
+}
+
+public class Query
+{
+    public int count { get; set; }
+    public Results results { get; set; }
+}
+
+public class RootObject
+{
+    public Query query { get; set; }
+}
